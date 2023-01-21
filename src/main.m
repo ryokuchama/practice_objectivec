01ff7424503c769aae4f56f67086cd1693f61c8f
ryokuchama <r.madrid.pg@gmail.com>
2023-01-21 17:09:11 +0900

    call class

--- src/main.m
+++ src/main.m
@@ -1,3 +1,13 @@
+#include <sample.h>
+
 int main() {
-    
+    // equivalent of new class
+    id type = [Sample alloc];
+    int code = [type sampleMethod: 5];
+    NSLog(@"main code: %d", code);
+    NSString *text = "text";
+    NSString *result = [type testMethod: text];
+    NSLog(@"main text: %@", result);
+    NSString *newMethod = [type newMethod];
+    NSLog(@"main new method: %@", newMethod);
 }
